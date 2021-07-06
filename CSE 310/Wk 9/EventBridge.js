d106693fe8cce0e7f50a2042ca12a8347688eefd
phi1ny3 <38825543+phi1ny3@users.noreply.github.com>
2021-07-05 21:37:34 -0600

    js

---
+++ CSE 310/Wk 9/EventBridge.js
@@ -0,0 +1,11 @@
+{
+  "source": [
+    "aws.cloudwatch"
+  ],
+  "detail-type": [
+    "CloudWatch Alarm State Change"
+  ],
+  "resources": [
+    "arn:aws:cloudwatch:us-east-1:407371014015:alarm:Exceed200"
+  ]
+}
